03ff5feb6aef860f9c767be3581fe62449d5008e
Fatima <fatima.hammouri@gmail.com>
2021-07-06 00:20:49 +0000

    draw_shape method

--- model.py
+++ model.py
@@ -25,6 +25,13 @@ class Shape():
         self.end_y = end_y
         self.fill_char = fill_char
 
+    def draw_shape(self):
+        width = x_end - x_start
+        height = y_end - y_start
+        for row in range(self.height):
+            print(self.fill_char * self.width)
 
+    # def change_fill_char(self, new_char): 
+    #     self.fill_char = new_char
 
     # def translate_rect(self, x_axis, y_axis, num):
