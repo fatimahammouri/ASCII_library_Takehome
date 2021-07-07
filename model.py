44b29cf0d3e84e44283bf9875941e261fb60e2d0
Fatima <fatima.hammouri@gmail.com>
2021-07-07 06:18:45 +0000

    shape methods

--- model.py
+++ model.py
@@ -9,15 +9,17 @@ class Canvas():
     def print_canvas(self):
         for row in range(self.height):
             print('*' * self.width)
+        # outer_list = [[0] * 4] * 3
             
 
     # def add_shape(self, Shape, x, y):
+       
 
     # def clear_all(self):
     
    
 
-class Shape():
+class Shape(Canvas(height, width)):
     def __init__(self, start_x, start_y, end_x, end_y, fill_char):
         self.start_x = start_x
         self.start_y = start_y
@@ -31,7 +33,12 @@ class Shape():
         for row in range(height):
             print(self.fill_char * width)
 
-    # def change_fill_char(self, new_char): 
-    #     self.fill_char = new_char
+    def change_fill_char(self, new_char): 
+        self.fill_char = new_char
 
-    # def translate_rect(self, x_axis, y_axis, num):
+    def translate_rect(self, x_axis, y_axis, num):
+        self.start_x = self.start_x + x_axis
+        self.start_y = self.start_y + y_axis
+        self.end_x = self.end_x + x_axis
+        self.end_y = self.end_y + y_axis
+ 
